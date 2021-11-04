242381fa2ff70703af95588d360a39c4c9fae28f
unknown <albertwtyan@163.com>
2021-11-04 21:28:19 +0800

    add element ui

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@ import App from './App.vue'
 // import './index.css'
 // import axios from 'axios';
 // import VueAxios from 'vue-axios';
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+// import Antd from 'ant-design-vue';
+// import 'ant-design-vue/dist/antd.css';
+import 'element-plus/dist/index.css'
+
 import draggable from 'vuedraggable'
 import {router} from './router'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
-app.use(Antd);
+// app.use(Antd);
+app.use(ElementPlus)
+
 app.use(draggable);
 app.use(router);
 app.mount('#app')
